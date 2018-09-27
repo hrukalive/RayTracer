e863a7b15176d63c21479888b719ffde539d29ca
hrukalive <bowen.parallife@gmail.com>
2018-09-27 13:42:14 -0500

    add components to gui

--- CppRewrite/Source/MainComponent.cpp
+++ CppRewrite/Source/MainComponent.cpp
@@ -11,17 +11,166 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    *progress = 0.5;
+    
     menuBar.reset(new MenuBarComponent(this));
+    image.reset(new ImageComponent());
+    progressBar.reset(new ProgressBar(*progress));
+    
+    progressBar->setSize(0, 25);
+    
     addAndMakeVisible(menuBar.get());
+    addAndMakeVisible(image.get());
+    addAndMakeVisible(progressBar.get());
+    
+#if JUCE_MAC
+    MenuBarModel::setMacMainMenu(this);
+    menuBar->setVisible(false);
+#else
+    menuBar->setVisible(true);
+#endif
+    progressBar->setVisible(true);
+    
     setApplicationCommandManagerToWatch(&commandManager);
     commandManager.registerAllCommandsForTarget(this);
     addKeyListener(commandManager.getKeyMappings());
-    
-    setSize (600, 400);
+#if JUCE_MAC
+    setSize(600, 400 + progressBar->getHeight());
+#else
+    setSize(600, 400 + progressBar->getHeight() + LookAndFeel::getDefaultLookAndFeel().getDefaultMenuBarHeight());
+#endif
 }
 
 MainComponent::~MainComponent()
 {
+#if JUCE_MAC
+    MenuBarModel::setMacMainMenu (nullptr);
+#endif
+}
+
+//==============================================================================
+StringArray MainComponent::getMenuBarNames()
+{
+    return { "Settings", "Render" };
+}
+
+PopupMenu MainComponent::getMenuForIndex (int menuIndex, const String& /*menuName*/)
+{
+    PopupMenu menu;
+    
+    if (menuIndex == 0)
+    {
+        menu.addCommandItem (&commandManager, CommandIDs::settingWorld);
+        menu.addCommandItem (&commandManager, CommandIDs::settingViewplane);
+        menu.addCommandItem (&commandManager, CommandIDs::settingCamera);
+        menu.addCommandItem (&commandManager, CommandIDs::settingLight);
+        menu.addCommandItem (&commandManager, CommandIDs::settingGeometry);
+        menu.addCommandItem (&commandManager, CommandIDs::settingMaterial);
+        menu.addCommandItem (&commandManager, CommandIDs::settingMesh);
+    }
+    else if (menuIndex == 1)
+    {
+        menu.addCommandItem (&commandManager, CommandIDs::startRender);
+        menu.addCommandItem (&commandManager, CommandIDs::pauseRender);
+        menu.addCommandItem (&commandManager, CommandIDs::stopRender);
+    }
+    
+    return menu;
+}
+
+//==============================================================================
+ApplicationCommandTarget* MainComponent::getNextCommandTarget()
+{
+    return findFirstTargetParentComponent();
+}
+void MainComponent::getAllCommands (Array<CommandID>& c)
+{
+    Array<CommandID> commands { CommandIDs::startRender,
+        CommandIDs::pauseRender,
+        CommandIDs::stopRender,
+        CommandIDs::settingMesh,
+        CommandIDs::settingMaterial,
+        CommandIDs::settingLight,
+        CommandIDs::settingCamera,
+        CommandIDs::settingViewplane,
+        CommandIDs::settingWorld,
+        CommandIDs::settingGeometry };
+    c.addArray (commands);
+}
+
+void MainComponent::getCommandInfo (CommandID commandID, ApplicationCommandInfo& result)
+{
+    switch (commandID)
+    {
+        case CommandIDs::startRender:
+            result.setInfo ("Start", "Initiate the rendering process", "Render", 0);
+#if JUCE_MAC
+            result.addDefaultKeypress ('r', ModifierKeys::commandModifier);
+#else
+            result.addDefaultKeypress ('r', ModifierKeys::ctrlModifier);
+#endif
+            break;
+        case CommandIDs::pauseRender:
+            result.setInfo ("Pause", "Pause the rendering process", "Render", 0);
+            break;
+        case CommandIDs::stopRender:
+            result.setInfo ("Stop", "Stop the rendering process", "Render", 0);
+            break;
+        case CommandIDs::settingGeometry:
+            result.setInfo ("Geometry", "Geometry settings", "Settings", 0);
+            break;
+        case CommandIDs::settingMesh:
+            result.setInfo ("Mesh", "Mesh settings", "Settings", 0);
+            break;
+        case CommandIDs::settingMaterial:
+            result.setInfo ("Material", "Material settings", "Settings", 0);
+            break;
+        case CommandIDs::settingLight:
+            result.setInfo ("Light", "Light settings", "Settings", 0);
+            break;
+        case CommandIDs::settingCamera:
+            result.setInfo ("Camera", "Camera settings", "Settings", 0);
+            break;
+        case CommandIDs::settingViewplane:
+            result.setInfo ("Viewplane", "Viewplane settings", "Settings", 0);
+            break;
+        case CommandIDs::settingWorld:
+            result.setInfo ("World", "World settings", "Settings", 0);
+            break;
+        default:
+            break;
+    }
+}
+
+bool MainComponent::perform (const InvocationInfo& info)
+{
+    switch (info.commandID)
+    {
+        case CommandIDs::startRender:
+            break;
+        case CommandIDs::pauseRender:
+            break;
+        case CommandIDs::stopRender:
+            break;
+        case CommandIDs::settingGeometry:
+            break;
+        case CommandIDs::settingMesh:
+            break;
+        case CommandIDs::settingMaterial:
+            break;
+        case CommandIDs::settingLight:
+            break;
+        case CommandIDs::settingCamera:
+            break;
+        case CommandIDs::settingViewplane:
+            break;
+        case CommandIDs::settingWorld:
+            break;
+        default:
+            return false;
+    }
+    
+    return true;
 }
 
 //==============================================================================
@@ -40,4 +189,10 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    auto b = getLocalBounds();
+#if !JUCE_MAC
+    menuBar->setBounds (b.removeFromTop (LookAndFeel::getDefaultLookAndFeel().getDefaultMenuBarHeight()));
+#endif
+    image->setBounds(b.withHeight(400));
+    progressBar->setBounds(b.removeFromBottom(progressBar->getHeight()));
 }
--- CppRewrite/Source/MainComponent.h
+++ CppRewrite/Source/MainComponent.h
@@ -22,7 +22,7 @@ class MainComponent   : public Component,
 public:
     enum CommandIDs
     {
-        startRender,
+        startRender = 1,
         pauseRender,
         stopRender,
         settingWorld,
@@ -30,12 +30,24 @@ public:
         settingCamera,
         settingViewplane,
         settingMesh,
+        settingGeometry,
         settingMaterial
     };
     //==============================================================================
     MainComponent();
     ~MainComponent();
 
+    //==============================================================================
+    StringArray getMenuBarNames() override;
+    PopupMenu getMenuForIndex (int menuIndex, const String& /*menuName*/) override;
+    void menuItemSelected (int /*menuItemID*/, int /*topLevelMenuIndex*/) override {};
+    
+    //==============================================================================
+    ApplicationCommandTarget* getNextCommandTarget() override;
+    void getAllCommands (Array<CommandID>& c) override;
+    void getCommandInfo (CommandID commandID, ApplicationCommandInfo& result) override;
+    bool perform (const InvocationInfo& info) override;
+    
     //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
@@ -45,7 +57,9 @@ private:
     // Your private member variables go here...
     ApplicationCommandManager commandManager;
     std::unique_ptr<MenuBarComponent> menuBar;
-
+    std::unique_ptr<ImageComponent> image;
+    std::unique_ptr<ProgressBar> progressBar;
+    double* progress = new double;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };

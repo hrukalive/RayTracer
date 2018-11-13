872d8578af39fb4103185279bb919bd7e154b942
hrukalive <bowen.parallife@gmail.com>
2018-11-13 11:49:07 -0600

    .

--- Source/Core/Materials/Mapping.h
+++ Source/Core/Materials/Mapping.h
@@ -22,6 +22,7 @@ public:
 class SphericalMap : public Mapping
 {
 public:
+    virtual ~SphericalMap() = default;
     Point2D getTexelCoord(const Point3D hitPoint, const int hres, const int vres) const override
     {
         FP_TYPE theta = acos(hitPoint.y);
@@ -30,6 +31,29 @@ public:
             phi += TWO_PI;
         FP_TYPE u = phi * INV_TWO_PI;
         FP_TYPE v = 1 - theta * INV_PI;
+        return Point2D((hres - 1) * u, (vres - 1) * v);
+    }
+};
+
+class LightProbe : public Mapping
+{
+    bool isPanoramic = false;
+public:
+    virtual ~LightProbe() = default;
+    void setPanoramic(const bool panoramic)
+    {
+        isPanoramic = panoramic;
+    }
+    Point2D getTexelCoord(const Point3D hitPoint, const int hres, const int vres) const override
+    {
+        FP_TYPE x = hitPoint.x, y = hitPoint.y, z = hitPoint.z;
+        FP_TYPE d = sqrt(x * x + y * y);
+        FP_TYPE sinBeta = y / d;
+        FP_TYPE cosBeta = x / d;
+        FP_TYPE alpha = isPanoramic ? acos(-z) : acos(z);
+        FP_TYPE r = alpha * INV_PI;
+        FP_TYPE u = (1 + r * cosBeta) * 0.5;
+        FP_TYPE v = (1 + r * sinBeta) * 0.5;
         return Point2D((hres - 1) * u, (vres - 1) * v);
     }
 };
--- Source/Core/Materials/Texture.h
+++ Source/Core/Materials/Texture.h
@@ -18,3 +18,23 @@ public:
     virtual ~Texture() = default;
     virtual RGBColor getColor(const HitRecord& record) const = 0;
 };
+
+class Checker3D : public Texture
+{
+    RGBColor color1, color2;
+    FP_TYPE size;
+public:
+    virtual ~Checker3D() = default;
+    void setSize(const FP_TYPE newSize)
+    {
+        size = newSize;
+    }
+    void setColor1(const RGBColor newColor1)
+    {
+        color1 = newColor1;
+    }
+    void setColor2(const RGBColor newColor2)
+    {
+        color2 = newColor2;
+    }
+};
